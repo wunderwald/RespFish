0d647e08527b30d45c10ac6a7e609aa85743df20
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2026-04-10 11:46:24 +0200

    clean up

--- Code/app/renderer.js
+++ Code/app/renderer.js
@@ -25,7 +25,7 @@ import { GazeManager   } from "./modules/gazeCalibration.js";
 // Change this value to switch frontends.
 // 'visualizer' | 'game' | 'ibreath' | 'gazetest'
 
-const FRONTEND = 'gazetest';
+const FRONTEND = 'ibreath';
 
 // ── Gaze tracking ─────────────────────────────────────────────────────────────
 //
