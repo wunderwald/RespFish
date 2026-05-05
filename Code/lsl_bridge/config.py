17b52db9a0a7ef92288f34dd41dfd84fda6b3023
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2026-05-05 11:00:56 +0200

    add bridge / ws / lsl config

---
+++ Code/lsl_bridge/config.py
@@ -0,0 +1,8 @@
+WS_HOST            = "localhost"
+WS_PORT            = 8765
+MARKER_WS_PORT     = 9001
+MARKER_STREAM_NAME = "RespFishMarkers"
+
+DISCOVERY_WAIT     = 1.0   # seconds per resolve_streams call
+DISCOVERY_INTERVAL = 2.0   # extra sleep between discovery rounds
+PULL_TIMEOUT       = 2.0   # seconds to wait for a single sample
