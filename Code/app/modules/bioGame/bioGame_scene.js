868108ffbadbc381db19784c6cdff89c6342c771
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2026-07-17 11:29:51 +0200

    fix sound config

--- Code/app/modules/bioGame/bioGame_scene.js
+++ Code/app/modules/bioGame/bioGame_scene.js
@@ -23,8 +23,8 @@ export const SCENES = {
     },
 
     sounds: {
-      ambience: 'sounds/whale.wav',
-      noise:    'sounds/cute.wav',
+      ambience: 'sounds/cute.wav',
+      noise:    'sounds/wind.wav',
       collect:  ['sounds/collect1.wav', 'sounds/collect2.wav', 'sounds/collect3.wav', 'sounds/collect4.wav'],
       miss:     'sounds/miss.wav',
     },
